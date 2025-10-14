170981401c3e10e60894b306029adb20c4c59f67
Alex Hancock <alexhancock@block.xyz>
2025-10-14 16:28:57 -0400

    chore: clean up random unused files (#5166)

--- JOKES.md
+++
@@ -1,9 +0,0 @@
-# goose Jokes 🦢
-
-## Why did the goose become a developer?
-
-Because it wanted to help debug all those "fowl" errors in the code!
-
----
-
-*Feel free to add more goose-related programming humor below!*
--- goose-gui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Launcher script for goose GUI
-# Suppresses common GLib warnings that don't affect functionality
-
-cd /home/alfonsodg/Devel-Local/oss/goose/ui/desktop/out/goose-linux-x64
-./goose 2>&1 | grep -v "GLib-GObject" | grep -v "browser_main_loop"
